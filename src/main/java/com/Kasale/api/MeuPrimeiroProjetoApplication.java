e2c309dc17272cb2aa14228aaeec1c7c7ed74fc3
Rita Santos <ritabr@gmail.com>
2021-11-08 17:22:32 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/Kasale/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.Kasale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
